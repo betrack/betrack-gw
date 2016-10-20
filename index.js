dec0e89bad4b01b77956e4791486e7303a16f8c5
Javier Velazquez Traut <javier@cran.io>
2016-10-20 16:55:44 -0300

    Added tag post

--- index.js
+++ index.js
@@ -7,23 +7,35 @@ var board = new five.Board({
   io: new raspi()
 });
 
-var minutes = 0.5;
-var delay = minutes * 60 * 1000;
+var GWminutes = 0.5;
+var TAGminutes = 0.1;
 
 var temperature = 0.0;
 
 client.on('connect', function() {
   console.log("MQTT connected");
   setInterval(function() {
-    console.log('Post every ' + minutes + ' minutes.');
+    console.log('Gateway post every ' + GWminutes + ' minutes.');
     var time = new Date();
     time.setHours(time.getHours()-3);
     var state = time.toISOString()+",";
     state = state + temperature.toFixed(1);
   	state = state + ",-34.594113,-58.433810"; //Jufre 570, CABA
-    client.publish("gw/ab:ab:ab:ab:ab:ab", state)
+    client.publish("gw/ab:ab:ab:ab:ab:ab", state);
     console.log(state);
-  }, delay);
+  }, GWminutes * 60 * 1000);
+
+  var packet = 0;
+  setInterval(function() {
+    console.log('Tag post every ' + TAGminutes + ' minutes.');
+    var time = new Date();
+    time.setHours(time.getHours()-3);
+    var state = time.toISOString()+",";
+    state = state + temperature.toFixed(1);
+    state = state + ",85," + (packet++); //85 Batt
+    client.publish("tag/11:11:11:11:11:11/ab:ab:ab:ab:ab:ab", state);
+    console.log(state);
+  }, TAGminutes * 60 * 1000);
 });
 
 board.on("ready", function() {
