7f14b49f9b9b4b2b00d876b5b67fd90bdf641db5
Javier Velazquez Traut <javier@cran.io>
2016-10-20 16:20:37 -0300

    Added snippet for temp using johnny five

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://45.55.229.97');
 
+var raspi = require('raspi-io');
+var five = require('johnny-five');
+var board = new five.Board({
+  io: new raspi()
+});
 
 //time.setHours(time.getHours()-24);
 var minutes = 0.5;
@@ -11,11 +16,29 @@ client.on('connect', function() {
   setInterval(function() {
     console.log('Post every ' + minutes + ' minutes.');
     var time = new Date();
-    time.setHours(time.getHours()-3);  
-  	var state = time.toISOString()+","+getRandomArbitrary(5,10).toFixed(1)+",-34.594113,-58.433810"; //Jufre 570, CABA
+    time.setHours(time.getHours()-3);
+    var state = time.toISOString()+",";
+    if(temperature){
+      state = state + temperature.celsius.toFixed(1);
+    }
+    else{
+      state = state + getRandomArbitrary(5,10).toFixed(1);
+    }
+  	state = state + ",-34.594113,-58.433810"; //Jufre 570, CABA
     //client.publish("gw/ab:ab:ab:ab:ab:ab", state)
     console.log(state);
   }, delay);
+});
+
+board.on("ready", function() {
+  var temperature = new five.Thermometer({
+    controller: "BMP180",
+    freq: 250
+  });
+
+  temperature.on("change", function() {
+    console.log("Temperature change celsius: ", this.celsius);
+  });
 });
 
 // Returns a random number between min (inclusive) and max (exclusive)
