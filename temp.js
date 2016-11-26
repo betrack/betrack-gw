d7fb9a975e2fe4e0a77e5461286f80a69c1eaac8
Javier Velazquez Traut <javier@cran.io>
2016-11-25 21:20:06 -0300

    Forget about last two commits

--- temp.js
+++ temp.js
@@ -1,46 +1,27 @@
-try{
-  var raspi = require('raspi-io');
-  var five = require('johnny-five');
-  var board = new five.Board({
-    io: new raspi()
-  });
-}
-catch(err){
-  console.error(err);
-}
+var raspi = require('raspi-io');
+var five = require('johnny-five');
+var board = new five.Board({
+  io: new raspi()
+});
 
 var jsonfile = require('jsonfile');
 const file = "temp.json";
 module.exports = {
     file: file
 };
-try{
-  board.on("ready", function() {
-    try{
-      var thermometer = new five.Thermometer({
-        controller: "BMP180",
-        freq: 250
-      });
-    }
-    catch(err){
-      console.error(err);
-    }
 
-    try{
-      thermometer.on("change", function() {
-        console.log("Temperature change celsius: ", this.celsius);
-        var json = {temp: this.celsius};
-        jsonfile.writeFile(file,json,function(err){
-          if(err)
-            console.error(err);
-        });
-      });
-    }
-    catch(err){
-      console.error(err);
-    }
+board.on("ready", function() {
+  var thermometer = new five.Thermometer({
+    controller: "BMP180",
+    freq: 250
   });
-}
-catch(err){
-  console.error(err);
-}    
+
+  thermometer.on("change", function() {
+    console.log("Temperature change celsius: ", this.celsius);
+    var json = {temp: this.celsius};
+    jsonfile.writeFile(file,json,function(err){
+      if(err)
+        console.error(err);
+    });
+  });
+});
