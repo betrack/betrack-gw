4ab5ee37a2da84d2f56f97e283d19171ca02b9ac
Javier Velazquez Traut <javier@cran.io>
2016-11-25 21:18:22 -0300

    Avoiding errors on mac osx

--- temp.js
+++ temp.js
@@ -14,24 +14,33 @@ const file = "temp.json";
 module.exports = {
     file: file
 };
-
 try{
   board.on("ready", function() {
-    var thermometer = new five.Thermometer({
-      controller: "BMP180",
-      freq: 250
-    });
-
-    thermometer.on("change", function() {
-      console.log("Temperature change celsius: ", this.celsius);
-      var json = {temp: this.celsius};
-      jsonfile.writeFile(file,json,function(err){
-        if(err)
-          console.error(err);
+    try{
+      var thermometer = new five.Thermometer({
+        controller: "BMP180",
+        freq: 250
       });
-    });
+    }
+    catch(err){
+      console.error(err);
+    }
+
+    try{
+      thermometer.on("change", function() {
+        console.log("Temperature change celsius: ", this.celsius);
+        var json = {temp: this.celsius};
+        jsonfile.writeFile(file,json,function(err){
+          if(err)
+            console.error(err);
+        });
+      });
+    }
+    catch(err){
+      console.error(err);
+    }
   });
 }
 catch(err){
   console.error(err);
-}
+}    
