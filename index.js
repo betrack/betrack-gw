96751f0514bff8b45444a88b3f8ea3c3e04ff1d8
Javier Velazquez Traut <javier@cran.io>
2016-12-01 16:41:33 -0300

    Using chokidr instead of fsmonitor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://45.55.229.97');
 var jsonfile = require('jsonfile');
-var fsmonitor = require('fsmonitor');
+var chokidar = require('chokidar');
 var del = require('node-delete');
 
 var bt = require('./bt.js');
@@ -9,56 +9,51 @@ var location = require('./location');
 var temperature = require('./temp');
 
 var GWminutes = 0.1;
-var TAGminutes = 1;
+var TAGminutes = 0.2;
 
 client.on('connect', function() {
   console.log("MQTT connected");
 
-  fsmonitor.watch('gw', null, function(change) {
-    change.addedFiles.forEach(function(file){
-      jsonfile.readFile('gw/'+ file, function(err, obj) {
-        if(err)
-          console.log(err);
-        else{
-          var state = obj.time+","+obj.temp.toFixed(1)+","+obj.lat.toFixed(7)+","+obj.lon.toFixed(7);
-          client.publish("gw/"+bt.address, state);
-          console.log(bt.address, state);
-          del('gw/' + file, function (err, paths) {
-            console.log('Deleted file', paths);
-          });
-        }
-      });
+  chokidar.watch('gw',{
+    persistent: true,
+    ignored: /[\/\\]\./,
+    ignoreInitial: false,
+    awaitWriteFinish: true
+  }).on('add', function(event, path) {
+    console.log(event);
+    jsonfile.readFile(event, function(err, obj) {
+      if(err)
+        console.log(err);
+      else{
+        var state = obj.time+","+obj.temp.toFixed(1)+","+obj.lat.toFixed(7)+","+obj.lon.toFixed(7);
+        client.publish("gw/"+bt.address, state);
+        console.log(bt.address, state);
+        del(event, function (err, files) {
+          //console.log('Deleted file', files);
+        });
+      }
     });
   });
 
-  fsmonitor.watch('tag', null, function(change) {
-    change.addedFiles.forEach(function(file){
-      jsonfile.readFile('tag/'+ file, function(err, obj) {
-        if(err)
-          console.log(err);
-        else{
-          var state = obj.time+","+obj.temp.toFixed(1)+","+obj.batt.toFixed(1)+","+obj.packet;
-          client.publish("tag/"+obj.address+"/"+bt.address, state);
-          console.log(obj.address,bt.address, state);
-          del('tag/' + file, function (err, paths) {
-            console.log('Deleted file', paths);
-          });
-        }
-      });
+  chokidar.watch('tag',{
+    persistent: true,
+    ignored: /[\/\\]\./,
+    ignoreInitial: false
+  }).on('add', function(event, path) {
+    console.log(event);
+    jsonfile.readFile(event, function(err, obj) {
+      if(err)
+        console.log(err);
+      else{
+        var state = obj.time+","+obj.temp.toFixed(1)+","+obj.batt.toFixed(1)+","+obj.packet;
+        client.publish("tag/"+obj.address+"/"+bt.address, state);
+        console.log(obj.address,bt.address, state);
+        del(event, function (err, files) {
+          //console.log('Deleted file', files);
+        });
+      }
     });
   });
-
-  var packet = 0;
-  setInterval(function() {
-    console.log('Tag post every ' + TAGminutes + ' minutes.');
-    var time = new Date();
-    time.setHours(time.getHours()-3);
-    var state = time.toISOString()+",";
-    state = state + '10.0';
-    state = state + ",85," + (packet++); //85 Batt
-    client.publish("tag/11:11:11:11:11:11/"+bt.address, state);
-    console.log(bt.address,state);
-  }, TAGminutes * 60 * 1000);
 });
 
 client.on('error', function(error) {
@@ -76,3 +71,16 @@ setInterval(function() {
       console.error(err);
   });
 }, GWminutes * 60 * 1000);
+
+var packet = 0;
+setInterval(function() {
+  console.log('Tag post every ' + TAGminutes + ' minutes.');
+  var time = new Date();
+  var timestamp = time.toUTCString();
+  time.setHours(time.getHours()-3);
+  var json = {address:"11:11:11:11:11:11", time: time.toISOString(), temp: 10.0, batt: 85, packet: (packet++)};
+  jsonfile.writeFile('tag/'+ timestamp + '.json',json,function(err){
+    if(err)
+      console.error(err);
+  });
+}, TAGminutes * 60 * 1000);
